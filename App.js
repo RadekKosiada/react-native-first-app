cc67921b2f6e2640198fe4ee707a4e62ea2e3351
RadekKosiada <kosiadar@gmail.com>
2020-12-02 08:48:51 +0100

    Add basic components w/props & state

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
+
+const getFullName = (firstName, secondName) => {
+  return firstName + ' ' + secondName;
+}
+
+const PresentationAndInput = (props) => {
+
+  return (
+    <View>
+      <Text>{props.greeting} {getFullName('Groovy', 'Goose')}</Text>
+      <TextInput style={styles.input}/>
+    </View>
+  ) 
+}
+
+const Song = (props) => {
+  //const[getter, setter] = useState(initialValue);
+  const[isPlayed, setIsPlayed] = useState(true);
+
+  return (
+    <View>
+      <Text>
+        This is a song {props.title}, and it's {isPlayed ? "being played" : "waiting on a playlist"}!
+      </Text>
+      <Button
+      //onPress is a prop
+        onPress={() => {
+          setIsPlayed(false);
+        }}
+        disabled={!isPlayed}
+        title={isPlayed ? "Play that song!" : "Mmm good vibes!"}
+      />
+    </View>
+  )
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hallo Welt!</Text>
+      <PresentationAndInput greeting="Hello"/>
+      <PresentationAndInput greeting="Witaj"/>
+      <Image 
+        source={{url: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+        style={{width: 200, height: 200}}
+       />
+       <Song title="Celebration"/>
+       <Song title="I want your love"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +61,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1
   },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
+
+
+// no index.html
+// CSS, style only in the file?
+// why at Image double {{}} bo tak
