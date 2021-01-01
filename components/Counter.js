764d23bbde9057b2852f2f04e52550f00bccb48d
RadekKosiada <kosiadar@gmail.com>
2021-01-01 17:47:22 +0100

    Add toggleColor() to change button color w/press

--- components/Counter.js
+++ components/Counter.js
@@ -5,15 +5,18 @@ import styles from './styles';
 const Counter = () => {
   // const [clicks, setClicks] = useState(0);
   const [click, setClicks] = useState(0);
-  
+  const [isGreen, setGreen] = useState(false);
+
   const incrementCounter = () => setClicks(click + 1);
+  const toggleColor = () => setGreen(!isGreen);
 
   return (
     <View>
       <Text>You clicked: {click}  </Text>
       <Button 
         title="Click me!"
-        onPress={incrementCounter}
+        onPress={toggleColor}
+        color = {isGreen ? 'green' : 'red'}
         
       />
 
