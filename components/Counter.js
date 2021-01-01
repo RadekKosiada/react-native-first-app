2216dce9877ba258d421d97eccc7a7daea1f89dd
RadekKosiada <kosiadar@gmail.com>
2021-01-01 17:43:53 +0100

    Export seClicks to a function

--- components/Counter.js
+++ components/Counter.js
@@ -5,13 +5,15 @@ import styles from './styles';
 const Counter = () => {
   // const [clicks, setClicks] = useState(0);
   const [click, setClicks] = useState(0);
+  
+  const incrementCounter = () => setClicks(click + 1);
 
   return (
     <View>
       <Text>You clicked: {click}  </Text>
       <Button 
         title="Click me!"
-        onPress={()=> setClicks(click + 1) }
+        onPress={incrementCounter}
         
       />
 
